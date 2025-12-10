4d0c2edd27017ddaf3414544031511e5c22f36bb
Alexander Kusakin <pm12.kusakin@gmail.com>
2025-12-11 02:30:55 +0700

    remove multy user

--- py-bot/add-user.py
+++ py-bot/add-user.py
@@ -5,9 +5,9 @@ import string
 chars = string.ascii_letters + string.digits + "+/="
 password = ''.join(secrets.choice(chars) for _ in range(32))
 
-# prod
-with open('/etc/hysteria/users.txt', 'a') as f:
-    f.write(password + '\n')
+# # prod
+# with open('/etc/hysteria/users.txt', 'a') as f:
+#     f.write(password + '\n')
 
 # # local
 # with open('./users.txt', 'a') as f:
@@ -15,5 +15,8 @@ with open('/etc/hysteria/users.txt', 'a') as f:
 
 
 # Вывод результата
-print(f"Password: {password}")
-print(f"Link: hy2://{password}@senator.giize.com:443?sni=www.github.com#MasqueradeVPN")
+password = "algFNdE82VhJulMxov+JMdg5"
+password2 = "Zpov2bBbv7h916kp2/z2Cw=="
+print(f"hy2://{password}@senator.giize.com:443?sni=www.github.com#MasqueradeVPN")
+print("----")
+print(f"hy2://{password}@senator.giize.com:443?obfs-password={password2}#ObfsVPN")
